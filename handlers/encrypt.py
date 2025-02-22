acf4f2f38cd88aedfb8cb8a73d81ab0346d6ac4e
WakeyHome <flpflpfli@gmail.com>
2025-02-23 03:15:07 +0700

    callbacks

--- handlers/encrypt.py
+++ handlers/encrypt.py
@@ -1,10 +1,15 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from keyboards.all_keyboards import greet_kb
 
 encryrouter = Router()
 
 @encryrouter.message(F.text == 'Настройки ⚙️')
 async def cmd_start_3(message: Message):
+    #calling inline keyboard in answer.look line 11, argument 2.
     await message.answer('потенциальные настройки')
+
+@encryrouter.callback_query(F.data == 'cryptmesg')
+async def receive_message_for_encrypt(call: CallbackQuery):
+    await call.message.answer() #looks like that i need to receive message, save it, format it and send the answer?
--- keyboards/all_keyboards.py
+++ keyboards/all_keyboards.py
@@ -5,4 +5,9 @@ from aiogram.types import ReplyKeyboardRemove, \
 f = [[KeyboardButton(text='Зашифровать 🔑'), KeyboardButton(text='Расшифровать 🔐'), KeyboardButton(text='Настройки ⚙️')]]
 
 greet_kb = ReplyKeyboardMarkup(keyboard=f)
+
+def crypto_inline_greet():
+    inline_kb_list = [
+        [InlineKeyboardButton(text='Сообщение', callback_data='cryptmesg')]
+    ]
 #greet_kb.add(button_hi)
