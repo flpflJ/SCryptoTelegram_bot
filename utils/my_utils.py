842384cae8d7bd81ca6d321204fcd343d3709ab5
flpflJ <flpflpfli@gmail.com>
2025-03-17 02:05:31 +0700

    +gronsfeld,vigenere,playfair

--- utils/my_utils.py
+++ utils/my_utils.py
@@ -95,11 +95,144 @@ def richelieu(message,key,flag):
     except TypeError:
         return 'Некорректный ключ, либо нет сообщения.'
 
-def gronsfield_cypher(message,key,flag):
-    raise Exception("not implemented yet")
+def gronsfield_cipher(message,key: str,flag):
+    alph = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    pattern = r'^\d+$'
+    if not re.fullmatch(pattern, key):
+        return 'Некорректный ключ. Это может быть только число.'
+    message_formatted = []
+    for i in range(len(message)):
+        if message[i] in alph:
+            message_formatted.append(alph.find(message[i]))
+    len_key = (len(message_formatted) // len(key)) * key + key[:(
+                len(message_formatted) % len(key))]
+    res = ''
+    if flag == 0:
+        for i in range(len(message_formatted)):
+            message_formatted[i] = (int(message_formatted[i]) + int(len_key[i])) % len(alph)
+            res += alph[message_formatted[i]]
+    else:
+        for i in range(len(message_formatted)):
+            message_formatted[i] = (int(message_formatted[i]) - int(len_key[i])) % len(alph)
+            res += alph[message_formatted[i]]
+    return res
 
-def vigenere_cypher(message,key,flag):
-    raise Exception("not implemented yet")
+def vigenere_cipher(message,key,flag):
+    alph = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    message_formatted = []
+    k = []
+    key_formatted = ''
+    for i in range(len(message)):
+        if message[i] in alph:
+            message_formatted.append(alph.find(message[i]))
+    for i in range(len(key)):
+        if key[i] in alph:
+            key_formatted+=key[i]
+    len_key = (len(message_formatted) // len(key_formatted)) * key_formatted + key_formatted[:(len(message_formatted)%len(key_formatted))]
+    for i in range(len(len_key)):
+        if len_key[i] in alph:
+            k.append(alph.find(len_key[i]))
+    result = []
+    if flag == 0:
+        for i in range(len(k)):
+            result.append((message_formatted[i]+k[i])%len(alph))
+    else:
+        for i in range(len(k)):
+            result.append((message_formatted[i]-k[i])%len(alph))
+    res = ''
+    for _ in result:
+        res += alph[_]
+    for i in range(len(message)):
+        if message[i] not in alph:
+            res = res[:i] + message[i] + res[i:]
+    return res
 
-def playfair_cypher(message,key,flag):
-    raise Exception("not implemented yet")
+def split_bigrams(text):
+    mas = []
+    strk = ""
+    for k in text:
+        if len(strk) == 0:
+            strk += k
+            if k == text[-1]:
+                mas.append(k + '~')
+        else:
+            if(strk[0] == k):
+                strk += '~'
+                mas.append(strk)
+                strk = k
+            else:
+                strk+= k
+                mas.append(strk)
+                strk = ""
+    return mas
+
+def form_matrix(alph, text):
+    unique_chars = set()
+    for alphab in alph.values():
+        unique_chars.update(alphab)
+    unique_chars = sorted(unique_chars)
+    pattern = f"^[{''.join(re.escape(c) for c in unique_chars)}]+$"
+    if not re.fullmatch(pattern, text):
+        return 'Использован недопустимый алфавит в сообщении или ключе'
+    for char in "".join(dict.fromkeys(text)):
+        unique_chars.remove(char)
+    return unique_chars
+
+def playfair_cipher(message,key,flag):
+    if flag == 1 and len(message) % 2 != 0:
+        return 'Неверная длина сообщения'
+    alphabets = {
+        "ru": "абвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+        "RU": "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+        "en": "abcdefghijklmnopqrstuvwxyz",
+        "EN": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "digits": "0123456789",
+        "specs": " ,.!@#$%^&*()-=_+<>?/~"
+    }
+    matrix = list("".join(dict.fromkeys(key))) + form_matrix(alphabets, key)
+    key_list = []
+    bigrams = split_bigrams(message)
+    res = ''
+    for i in range(15):
+        tmp = []
+        for j in range(10):
+            tmp.append(matrix[i*10+j])
+        key_list.append(tmp)
+    matrix = key_list
+    for bigram in bigrams:
+        tmp_b = ''
+        x0,y0,x1,y1,f0,f1 = (0,)*6
+        for i in range(15):
+            for j in range(10):
+                if bigram[0] == matrix[i][j]:
+                    x0,y0=i,j
+                    f0 = 1
+                if bigram[1] == matrix[i][j]:
+                    x1,y1=i,j
+                    f1=1
+                if f0 and f1:
+                    break
+            if f0 and f1:
+                break
+        if x0 == x1:
+            if(flag == 0):
+                tmp_b += matrix[x0][(y0+1)%10]
+                tmp_b += matrix[x1][(y1+1)%10]
+            else:
+                tmp_b += matrix[x0][(y0-1)%10]
+                tmp_b += matrix[x1][(y1-1)%10]
+        elif y0 == y1:
+            if flag == 0:
+                tmp_b += matrix[(x0+1)%15][y0]
+                tmp_b += matrix[(x1+1)%15][y1]
+            else:
+                tmp_b += matrix[(x0-1)%15][y0]
+                tmp_b += matrix[(x1-1)%15][y1]
+        else:
+            tmp_b += matrix[x0][y1]
+            tmp_b += matrix[x1][y0]
+        res += tmp_b
+    if flag == 0:
+        return res
+    else:
+        return res.replace('~','')
